b16d269c4176d67de3f10a149bc3e5a1465274ff
Andrey <86668671+Hostill@users.noreply.github.com>
2021-10-07 16:46:03 +0300

    prtf

---
+++ w-Portfolio/js/script.js
@@ -0,0 +1,21 @@
+
+//JS Scrypt
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document. querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+}); 
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+//JS Scrypt
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
